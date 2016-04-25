4908b9e14067a550760efc95b4c9474018d3f12e
blue <blue@tigrison.com>
2016-04-26 01:07:25 +0900

    2.1.1 공부내용 추가

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,17 +9,20 @@ import org.springframework.context.annotation.Import;
 import java.util.Scanner;
 
 @EnableAutoConfiguration
-@Import(AppConfig.class)
+@Import(AppConfig.class) 
+//JavaConfig를 읽어들이기 위해서 @Configuration이 붙은 클래스를 지정합니다.
 public class App {
     public static void main(String[] args) {
+    	//자바 7에서 도입한 try-with-resources 문 안에 변수를 선언하여 try안쪽에서 처리가 끝나면 자동으로 close()메소드가 호출되어 
+    	//DI컨테이너가 소멸되고 어플리케이션이 실행을 마치게 됩니다.
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
 
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in); //표준입력으로 데이터를 받아들인다.
             System.out.print("Enter 2 numbers like 'a b' : ");
             int a = scanner.nextInt();
             int b = scanner.nextInt();
 
-            Calculator calculator = context.getBean(Calculator.class);
+            Calculator calculator = context.getBean(Calculator.class); //getBean()메서드를 사용하여 DI컨테이너에서 Calculator 타입 인스턴스를 받습니다.
             int result = calculator.calc(a, b);
 
             System.out.println("result = " + result);
--- src/main/java/com/example/AppConfig.java
+++ src/main/java/com/example/AppConfig.java
@@ -5,10 +5,16 @@ import com.example.app.Calculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// 이 클래스가 JavaConfing용 클래스임을 컴파일러에 알립니다.
+@Configuration 
 public class AppConfig {
-    @Bean
+    /*
+     *DI 컨테이너가 관리할 Bean 을 생성하는 메서드에 붙입니다.
+     *기본적으로 메서드 이름이 Bean 이름입니다. 
+     *또한, 기본값으로 이 메서드가 생성한 인스턴스가 싱글통이 되며, DI컨테이너별로 인스턴스 한 개가 생성됩니다. 
+     */
+	@Bean
     Calculator calculator() {
-        return new AddCalculator();
+        return new AddCalculator(); //생성자로 생성한 Calculator 타입의 calculator 인스턴스를 DI 컨테이너가 싱글톤으로 관리합니다.
     }
 }
